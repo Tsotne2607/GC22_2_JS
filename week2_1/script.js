ddae1ccdf431897d24c651d0658c328e7d25c94a
LAB316 Student15 <LAB316_Student15@gau.local>
2019-09-27 14:40:29 +0400

    First COmmit in week 2

---
+++ week2_1/script.js
@@ -0,0 +1,16 @@
+function printHello(){
+    console.log('Hello, I am JavaScript');
+}
+
+function sumOfTwoNumbers(number1, number2){
+    console.log(number1+number2);
+}
+
+function getFive(){
+    return 5;
+}
+
+function returnSumOfTwoNumbers(number1, number2){
+    return number1+number2;
+
+}
